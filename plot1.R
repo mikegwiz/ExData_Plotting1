e105bd33225569ed7d4a9b8f0237940bb11c8231
Michael Giangrande <MichaelGiangrande@westat.com>
2019-04-30 10:18:15 -0400

    plot 1 update to code and output png

--- plot1.R
+++ plot1.R
@@ -7,8 +7,15 @@ dfile <- "./household_power_consumption.txt"
 data <- read.table(dfile, sep = ";", header = TRUE)
 
 # Filter data for 2007-02-01 and 2007-02-02
-data <- data %>% filter(Date == "2/1/2007" | Date == "2/2/2007")
+data <- data %>% filter(Date == "1/2/2007" | Date == "2/2/2007")
 
-# Convert date and time variables from factors
-data$Date <- as.Date(data$Date, format = "%m/%d/%Y")
-data$Time <- strptime(c(data$Time), format = "%H:%M:%S")
+# Convert date and time variables from factors in a new variable "datetime"
+data$datetime <- strptime(paste(as.character(data$Date), as.character(data$Time), sep = "."), format = "%d/%m/%Y.%H:%M:%S")
+
+# Create Plot 1
+plotdata <- as.numeric(as.character(data$Global_active_power))
+
+png("plot1.png")
+hist(plotdata, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+dev.off()
+
